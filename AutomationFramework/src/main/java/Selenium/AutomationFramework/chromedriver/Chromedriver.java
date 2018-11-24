80f13185ad444fac948376a8f5a39c27ac24fc70
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2018-11-23 23:58:08 -0600

    Chrome code + Selenium Xpath files, SQL, UNIX

--- AutomationFramework/src/main/java/Selenium/AutomationFramework/chromedriver/Chromedriver.java
+++ AutomationFramework/src/main/java/Selenium/AutomationFramework/chromedriver/Chromedriver.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 	//open Walgreens.com in chrome browser
 	//click login button, enter username & password
@@ -13,19 +15,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 		public class Chromedriver {
 
-	
-			public static void main(String[] args) {
+			WebDriver driver;
+			
+			@Test
+			public void setup() {
 				
 				File f = new File("chromedriver.exe");
 			    System.setProperty("webdriver.chrome.driver",f.getAbsolutePath());  
 			     
 				 WebDriver driver = new ChromeDriver();
-				 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+				 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 				 
-				 String url= "www.walgreens.com";
+				 String url= "https://www.google.com/?gws_rd=ssl";
 				 driver.get(url);
-				 				
+		
+				 String Actual = driver.getCurrentUrl();
+				 String Expected= "https://www.google.com/?gws_rd=ssl";
+				 
+				 Assert.assertEquals(Actual, Expected);
+				 System.out.println( "The URL for this site is " + Actual);
+				 
+				
+			}
+				 
+			
+					@Test
+					public void teardown() {
+					WebDriver driver = new ChromeDriver();
+					driver.quit();
 		
 			}
 		
-		}
+				 
+	}
