73aba17c483e632c640441e953e30c0cbfa1850f
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2018-11-29 00:22:13 -0600

    Page Object Model

---
+++ AutomationFramework/src/main/java/selenium/automationFramework/pageObjectModel/ExpediaPOM.java
@@ -0,0 +1,46 @@
+package selenium.automationFramework.pageObjectModel;
+
+import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import selenium.automationFramework.pageObjectModel.PageObjectModel;
+
+
+	public class ExpediaPOM {
+
+	private WebDriver driver;
+	private String baseUrl;
+//	static Logger log = Logger.getLogger(TestNG_TestSuite.class);
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		baseUrl = "https://www.expedia.com/";
+
+		// Maximize the browser's window
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		PropertyConfigurator.configure("log4j.properties");
+		driver.get(baseUrl);
+	}
+
+	@Test
+	public void fillBasicInfo() throws Exception {
+		PageObjectModel.navigateToFlightsTab(driver);
+		PageObjectModel.fillOriginTextBox(driver, "New York");
+		PageObjectModel.fillDestinationTextBox(driver, "Chicago");
+		PageObjectModel.fillDepartureDateTextBox(driver, "12/25/2015");
+		PageObjectModel.fillReturnDateTextBox(driver, "12/31/2015");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+}
---
+++ AutomationFramework/src/main/java/selenium/automationFramework/pageObjectModel/PageObjectModel.java
@@ -0,0 +1,126 @@
+package selenium.automationFramework.pageObjectModel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+
+	public class PageObjectModel {
+	
+		// Expedia.com Home Page
+		
+		public static WebElement element = null;
+		
+		/**
+		 * Returns the flight origin text box element
+		 * @param driver
+		 * @return
+		 */
+		public static WebElement originTextBox(WebDriver driver) {
+			element = driver.findElement(By.id("flight-origin"));
+			return element;
+		}
+		
+		public static void fillOriginTextBox(WebDriver driver, String origin) {
+			element = originTextBox(driver);
+			element.sendKeys(origin);
+		}
+
+		/**
+		 * Returns the flight destination text box element
+		 * @param driver
+		 * @return
+		 */
+		public static WebElement destinationTextBox(WebDriver driver) {
+			element = driver.findElement(By.id("flight-destination"));
+			return element;
+		}
+
+		
+		/**
+		 * Fills the destinationTextBox
+		 * @param driver 
+		 * 
+		 */
+		
+		public static void fillDestinationTextBox(WebDriver driver, String Destination) {
+			element = destinationTextBox(driver);
+			element.sendKeys(Destination);
+		}
+		
+		
+		/**
+		 * Returns the departure date text box element
+		 * @param driver
+		 * @return
+		 */
+		public static WebElement departureDateTextBox(WebDriver driver) {
+			element = driver.findElement(By.id("flight-departing"));
+			return element;
+		}
+
+		/**
+		 * Returns the return date text box element
+		 * @param driver
+		 * @return
+		 */
+		public static WebElement returnDateTextBox(WebDriver driver) {
+			element = driver.findElement(By.id("flight-returning"));
+			return element;
+		}
+
+		/**
+		 * Returns the search button box element
+		 * @param driver
+		 * @return
+		 */
+		public static WebElement searchButton(WebDriver driver) {
+			element = driver.findElement(By.id("search-button"));
+			return element;
+		}
+
+		/**
+		 * Click on search button
+		 * @param driver
+		 */
+		public static void clickOnSearchButton(WebDriver driver) {
+			element = searchButton(driver);
+			element.click();
+		}
+		
+		/**
+		 * Navigate to flights tab
+		 * @param driver
+		 */
+		public static void navigateToFlightsTab(WebDriver driver) {
+			driver.findElement(By.id("header-history")).click();
+			element = driver.findElement(By.id("tab-flight-tab"));
+			element.click();
+		}
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
