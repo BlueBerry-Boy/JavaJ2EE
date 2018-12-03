38d78b589f4d5ad2212a24e3577b4f66c0d868a9
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2018-12-03 17:20:05 -0600

    Enums

---
+++ AutomationFramework/src/main/java/selenium/automationFramework/enum_/Park.java
@@ -0,0 +1,43 @@
+package selenium.automationFramework.enum_;
+
+
+
+	public class Park {
+
+		public static void main(String[] args) {
+		Parking prk;
+		 
+		 prk = Parking.MONTHLY;
+		 
+		System.out.println("You selected " + prk + " Parking");
+		 
+		// System.out.println(Parking.VALET);
+		 
+		 
+		 switch(prk) {
+		 
+		 case ECONOMY:
+			 System.out.println("Economy parking is $10");
+			 break;
+			 
+		 case EXECUTIVE:
+			 System.out.println("Executive parking is $60");
+			  break;
+		
+		 case WEEKEND:	  
+			 System.out.println("Weekend parking is $40");
+			 break;
+			 
+		 case VALET:
+			 System.out.println("The Valet is $20");
+			 break;
+			 
+		 case MONTHLY:
+			 System.out.println("Monthly Parking is $100");
+			 break;
+			 
+		 }
+
+	}
+
+}
---
+++ AutomationFramework/src/main/java/selenium/automationFramework/enum_/Parking.java
@@ -0,0 +1,8 @@
+package selenium.automationFramework.enum_;
+
+
+
+
+	public enum Parking {
+		ECONOMY, EXECUTIVE, WEEKEND, VALET, MONTHLY
+	}
