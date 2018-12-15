302b345e0c606fa0dc1ac341c1b4b87fe10145f0
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2018-12-15 02:22:57 -0600

    selenium chromedriver

--- AutomationFramework/src/main/java/selenium/automationFramework/chromedriver/Chromedriver.java
+++ AutomationFramework/src/main/java/selenium/automationFramework/chromedriver/Chromedriver.java
@@ -5,12 +5,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 		public class Chromedriver {
-
-			WebDriver driver;
 			
 /**
  * Or Use local path in pc folders:
@@ -18,33 +17,39 @@ import org.testng.annotations.Test;
  * 			
  */
 			
-			@Test
-			public void setup() {
-						
-				File f = new File("chromedriver.exe");
-			    System.setProperty("webdriver.chrome.driver",f.getAbsolutePath());     
-     
-				 WebDriver driver = new ChromeDriver();
+				 @BeforeClass
+				 public void setup() {		
+			   	 File f = new File("chromedriver.exe");
+			     System.setProperty("webdriver.chrome.driver",f.getAbsolutePath());
+			     
+			     WebDriver driver = new ChromeDriver();
 				 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-				 
+				 driver.manage().window().maximize();
+					 
 				 String url= "https://www.google.com/?gws_rd=ssl";
-				 driver.get(url);
-		
+				 driver.get(url);  
+				 }
+				 
+				 @Test(priority=0)
+				 public void googleHomePage() {	 
+				 WebDriver driver = new ChromeDriver();
 				 String Actual = driver.getCurrentUrl();
-				 String Expected= "https://www.google.com/?gws_rd=ssl";
+				// String Expected= "https://www.google.com/?gws_rd=ssl";
 				 
-				 Assert.assertEquals(Actual, Expected);
-				 System.out.println( "The URL for this site is " + Actual);
-			
-			}
+				// Assert.assertEquals(Actual, Expected);
+				 System.out.println( "The URL for this site is " + Actual);		
+				 }
+					
+				@Test(priority=1)
+				public void close() {
+				WebDriver driver = new ChromeDriver();
+				driver.close();
+				}
 				 
-			
-					@Test
-					public void teardown() {
-					WebDriver driver = new ChromeDriver();
-					driver.quit();
-		
+				@AfterClass
+				public void teardown() {
+				WebDriver driver = new ChromeDriver();
+				driver.quit();	
 			}
-		
 				 
 	}
