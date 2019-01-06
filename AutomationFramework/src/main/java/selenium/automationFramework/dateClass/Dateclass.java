0eda2197b5ec9da22afcca37dca719f8fe4142a9
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2019-01-05 23:10:30 -0600

    Date Class

--- AutomationFramework/src/main/java/selenium/automationFramework/dateClass/Dateclass.java
+++ AutomationFramework/src/main/java/selenium/automationFramework/dateClass/Dateclass.java
@@ -13,13 +13,27 @@ public class Dateclass {
 		
 		// Parse Date into string
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		String datestr = "01/11/2018";
-		sdf.parse(datestr);	
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		String datestr = "01/21/2028";
 		
+		sdf.parse(datestr );	
 		Date D2 = sdf.parse(datestr);
-		
 		System.out.println("The formatted date is :" + D2);
+	
+		
+		//after method -Returns true if object contains a date that is later than the one specified by date
+
+		System.out.println("is d1 after D2? " + d1.after(D2));
+		
+		//before method - Returns true if object contains a date that is earlier than the one specified by date
+		
+		System.out.println("is d1 before D2? " + d1.before(D2));
+		
+		//compare method
+		d1.compareTo(D2);
+		
+		// toString converts date object into a String
+		d1.toString();
 		
 	}
 
