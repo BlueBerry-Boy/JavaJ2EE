035944d6c2f68cc835ba314da53e260c48fa8386
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2019-01-07 22:19:25 -0600

    Class with constant files

--- AutomationFramework/src/main/java/selenium/automationFramework/dataDrivenTest/Constants.java
+++ AutomationFramework/src/main/java/selenium/automationFramework/dataDrivenTest/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	
 	public static final String URL = "http://letskodeit.com";
     public static final String File_Path = "C://Users//Anonymous//Documents//QA//JAVA//Java Files & Selenium//Lets kode it//Data Driven Testing//";
-    public static final String File_Name = "Logins.xlsx";
+    public static final String File_Name = "Logins.xlsx";  //this is the Excel file name
 	
 }
