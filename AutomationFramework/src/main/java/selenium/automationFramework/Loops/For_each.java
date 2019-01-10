9364ac6fb501fae1ea6d7c29ef6db76074e7d6ab
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2019-01-09 23:41:22 -0600

    Loops & Pyramids

---
+++ AutomationFramework/src/main/java/selenium/automationFramework/Loops/For_each.java
@@ -0,0 +1,32 @@
+package selenium.automationFramework.Loops;
+
+public class For_each {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Enhanced For Loop Syntax
+		 * for(type iterationVariable : collection) statement block
+		 */
+
+		int nums [] = {1,2,3,4,5,6,7,8,9,10};
+		
+		int sum = 0;
+//	
+//		for(int i=0; i<10; i++)  sum += nums[i];
+//	
+//		System.out.println(sum);
+	
+		for(int s : nums)   
+		{
+			System.out.println(s);
+			sum += s;
+		}
+		
+		System.out.println("The total value is " + sum);
+	}
+
+
+}
+
+
---
+++ AutomationFramework/src/main/java/selenium/automationFramework/Loops/For_loop.java
@@ -0,0 +1,16 @@
+package selenium.automationFramework.Loops;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		
+		String J = " Repeat";
+		
+		for(int x = 0; x<J.length(); x++) {
+			
+			System.out.println(J);
+		}
+
+	}
+
+}
