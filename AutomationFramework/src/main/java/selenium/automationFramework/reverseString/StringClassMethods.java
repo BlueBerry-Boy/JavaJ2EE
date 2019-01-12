38468abdcba441704e73c3171398c9e71dc53194
Anonymous <Anonymous@LAPTOP-LDBHT6RJ>
2019-01-11 22:09:30 -0600

    String class methods

---
+++ AutomationFramework/src/main/java/selenium/automationFramework/reverseString/StringClassMethods.java
@@ -0,0 +1,43 @@
+package selenium.automationFramework.reverseString;
+
+public class StringClassMethods {
+
+	public static void main(String[] args) {
+		/**
+		 * We are going to see some uses of the methods in the string class
+		 * 
+		 **/
+		
+		String a = "Library";
+		
+	
+//charat()
+		System.out.println(a.charAt(2));
+		
+//indexof
+		
+		System.out.println(a.indexOf("y"));    //always use quotations "" with a String.
+		
+		
+//substring
+		
+		System.out.println(a.substring(0, 4));
+	
+//concat();
+	
+		
+		System.out.println(a.concat(" of Carrollton"));
+	
+		
+//length();
+	//	System.out.println(a.length);
+		
+	
+// split();
+	String arr [] = (a.split("r"));
+	System.out.println(arr[0]);
+	System.out.println(arr[1]);
+
+	}
+
+}
