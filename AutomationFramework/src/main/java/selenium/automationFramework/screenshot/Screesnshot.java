fa6ff737b0c7c536d8d9a5106d5b6c8962c4de92
Adif <45302860+SDET101@users.noreply.github.com>
2019-03-06 11:14:35 -0600

    Update Screesnshot.java

--- AutomationFramework/src/main/java/selenium/automationFramework/screenshot/Screesnshot.java
+++ AutomationFramework/src/main/java/selenium/automationFramework/screenshot/Screesnshot.java
@@ -37,3 +37,48 @@ import org.openqa.selenium.OutputType;
 		}
 
 }
+
+
+public class ScreenshotUtils {
+
+	/**
+	 * screenshot - Takes screenshot using Robot Class
+	 * @param folderPath
+	 * @param counter
+	 */
+	public static void screenshot(String folderPath, int counter){
+		
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = toolkit.getScreenSize();
+		Rectangle screenRect = new Rectangle(screenSize);
+		
+		Robot robot = null;
+	try {
+		 robot = new Robot();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	String sCounter = "0000"+String.valueOf(counter);
+	sCounter = sCounter.substring(sCounter.length()-4,sCounter.length());
+	
+	
+	// Save captured image
+	BufferedImage image = robot.createScreenCapture(screenRect);
+	
+
+	File dir = new File(folderPath);
+	if(!dir.exists())
+	dir.mkdirs();
+	 
+	try {
+		ImageIO.write(image, "jpg", new File(folderPath+ "/screenShot"+sCounter+".jpg"));
+	} catch (IOException e) {
+ 		e.printStackTrace();
+	}
+	
+	System.out.println("Screenshot is captured @ "+folderPath+ "/screenShot"+sCounter+".jpg");
+	
+	}
+
+}
